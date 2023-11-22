df1ac1144ff9bfe0caff0857fa45e4add0529dba
Houssem Ben Mabrouk <houssem.benmabrouk.ext@orange.com>
2023-11-22 11:34:15 +0100

    implement client_credentials grant type
    Signed-off-by: Houssem Ben Mabrouk <houssem.benmabrouk.ext@orange.com>

--- server/oauth2.go
+++ server/oauth2.go
@@ -93,7 +93,7 @@ func tokenErr(w http.ResponseWriter, typ, description string, statusCode int) er
 	return nil
 }
 
-//nolint
+// nolint
 const (
 	errInvalidRequest          = "invalid_request"
 	errUnauthorizedClient      = "unauthorized_client"
@@ -132,6 +132,7 @@ const (
 	grantTypeImplicit          = "implicit"
 	grantTypePassword          = "password"
 	grantTypeDeviceCode        = "urn:ietf:params:oauth:grant-type:device_code"
+	grantTypeClientCredentials = "client_credentials"
 )
 
 const (
